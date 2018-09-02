3d376f449521d4649299e72868a908245fcd64fa
Luke Griffith <lukemgriffith@gmail.com>
2018-09-02 15:31:15 +0100

    Moving structures to go package

--- service.dailySpend/main.go
+++ service.dailySpend/main.go
@@ -6,48 +6,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	//"net/url"
   "time"
+
+  "github.com/lukemgriffith/ynabReporting/service.dailySpend/spending"
 )
 
-type TransactionsEnvelope struct {
-	Data struct {
-		Transactions []struct {
-			ID                string        `json:"id"`
-			Date              string        `json:"date"`
-			Amount            int           `json:"amount"`
-			Memo              string        `json:"memo"`
-			Cleared           string        `json:"cleared"`
-			Approved          bool          `json:"approved"`
-			FlagColor         interface{}   `json:"flag_color"`
-			AccountID         string        `json:"account_id"`
-			AccountName       string        `json:"account_name"`
-			PayeeID           string        `json:"payee_id"`
-			PayeeName         string        `json:"payee_name"`
-			CategoryID        string        `json:"category_id"`
-			CategoryName      string        `json:"category_name"`
-			TransferAccountID string        `json:"transfer_account_id"`
-			ImportID          string        `json:"import_id"`
-			Deleted           bool          `json:"deleted"`
-			Subtransactions   []interface{} `json:"subtransactions"`
-		} `json:"transactions"`
-	} `json:"data"`
-}
 
 
-type DailySpending struct {
-
-  TotalSpend, TotalTransactions *int
-}
-
-func (s *DailySpending) AddTo(spend int) {
-
-  var spent = *s.TotalSpend + spend
-  var trans = *s.TotalTransactions + 1 
-
-  s.TotalSpend =  &spent
-  s.TotalTransactions = &trans
-}
 
 
 
