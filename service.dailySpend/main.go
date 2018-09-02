44c7f4dc150d3ef06a7c1fa6eee8a5489e5379ad
Luke Griffith <lukemgriffith@gmail.com>
2018-09-02 19:33:18 +0100

    made a webservice

--- service.dailySpend/main.go
+++ service.dailySpend/main.go
@@ -5,13 +5,24 @@ import (
 	"github.com/lukemgriffith/ynabReporting/service.dailySpend/Spending"
   "log"
   "encoding/json"
+  "net/http"
 )
 
 func main() {
 
+  http.HandleFunc("/", serveSpending)
+  if err := http.ListenAndServe(":8081", nil); err != nil {
+    panic(err)
+  }
+
+
+
+}
+
+func serveSpending(w http.ResponseWriter, r *http.Request) {
   cache_url := "http://localhost:8080/"
 
-	s, err := Spending.Get(cache_url)
+  s, err := Spending.Get(cache_url)
 
 	if err != nil {
 		log.Fatal("Process failed: ", err)
@@ -24,6 +35,8 @@ func main() {
     log.Fatal(err)
   }
 
-  fmt.Println(string(jsonString))
+  log.Print(r.Method, " at ", r.RequestURI, " from ", r.RemoteAddr)
+
+  w.Write(jsonString)
 
 }
