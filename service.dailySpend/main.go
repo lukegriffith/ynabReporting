d355864fb91add8271ab0cf1765764d7ae21bea8
Luke Griffith <lukemgriffith@gmail.com>
2018-09-03 23:03:52 +0100

    Refactored to controllers.go, and added 7days view.

--- service.dailySpend/main.go
+++ service.dailySpend/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 
 	"github.com/lukemgriffith/ynabReporting/service.dailySpend/Spending"
@@ -13,29 +11,9 @@ func main() {
 
 	_ = Spending.NewCacheClient(cache_url)
 
-	http.HandleFunc("/", serveSpending)
+	http.HandleFunc("/avgSpending", Spending.GetAverageDailySpending)
+	http.HandleFunc("/last7", Spending.GetLast7Days)
 	if err := http.ListenAndServe(":8081", nil); err != nil {
 		panic(err)
 	}
-
-}
-
-func serveSpending(w http.ResponseWriter, r *http.Request) {
-
-	s, err := Spending.Get()
-
-	if err != nil {
-		log.Fatal("Process failed: ", err)
-	}
-
-	jsonString, err := json.Marshal(s)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Print(r.Method, " at ", r.RequestURI, " from ", r.RemoteAddr)
-
-	w.Write(jsonString)
-
 }
